549c683be5429f47e98f1945fb27f8cc6fcc8ce4
Premkumar-max <premarun163@gmail.com>
2025-03-03 23:25:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "premterraform-s3-bucket"
+    bucket = "prem-s3-buckets3"
     key    = "vgs/terraform.tfstate"
     region = "us-east-1"   
   }
