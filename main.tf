e96118ef5fdb87c6c9007746d125096dd000126c
Premkumar-max <premarun163@gmail.com>
2025-03-03 22:13:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Create EC2 Instance
-resource "aws_instance" "my-ec2-vm" {
+#*resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-00bb6a80f01f03502"  #Ubuntu
   #ami               = "ami-0d2614eafc1b0e4d2"  #windows
   instance_type     = "t2.micro"
@@ -9,5 +9,5 @@ resource "aws_instance" "my-ec2-vm" {
      "Name" = "web"
     #"tag1" = "Update-test-1"    
   }
-}
+}*/
 
